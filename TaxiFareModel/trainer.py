bb45108870066451e42e15cc830947084ac9d1c5
Daniel Bajik <danielbajik@hotmail.com>
2022-05-31 20:02:15 -0300

    Progress in Challenge 5

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,12 +1,21 @@
-# imports
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from memoized_property import memoized_property
+import mlflow
+from mlflow.tracking import MlflowClient
 
+from TaxiFareModel.data import clean_data, get_data
 from TaxiFareModel.encoders import DistanceTransformer
 from TaxiFareModel.encoders import TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
+
+import joblib
+
+MLFLOW_URI = 'https://mlflow.lewagon.ai/'
+EXPERIMENT_NAME = "[AR] [Buenos Aires] [DanielBajik] TaxiFareMode@1.1"
 
 class Trainer():
     def __init__(self, X, y):
@@ -17,6 +26,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -37,6 +47,9 @@ class Trainer():
             ('preproc', preproc_pipe),
             ('linear_model', LinearRegression())
         ])
+
+        self.mlflow_log_param("MODEL NAME", LinearRegression.__name__)
+
         return self.pipeline        
 
        # implement train() function
@@ -51,14 +64,40 @@ class Trainer():
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse", rmse)
         return rmse
+    
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
 
-from TaxiFareModel.data import get_data, clean_data
-from sklearn.model_selection import train_test_split 
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, "model.joblib")
+        
 
 if __name__ == "__main__":
+
     # store the data in a DataFrame
-    df = get_data()
+    df = clean_data(get_data())
 
     # set X and y
     y = df["fare_amount"]
@@ -67,16 +106,18 @@ if __name__ == "__main__":
     # hold out
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
 
-    #
-
-    trainer=Trainer(X_train,y_train)
-
     # build pipeline
-    pipeline = trainer.set_pipeline()
+    trainer = Trainer(X_train, y_train)
+    trainer.set_pipeline()
 
     # train the pipeline
     trainer.run()
 
     # evaluate the pipeline
     rmse = trainer.evaluate(X_val, y_val)
+    
+    trainer.save_model
+
     print(rmse)
+
+    print(f"experiment URL: https://mlflow.lewagon.ai/#/experiments/{trainer.mlflow_experiment_id}")
